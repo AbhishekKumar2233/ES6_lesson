57d3d1072b321c0fce0ad816fc192dd0b27357b7
Abhishek Kumar <67472833+AbhishekKumar2233@users.noreply.github.com>
2022-01-14 23:22:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 // import pi, {doublePi,triplePi} from "./math.js";
 import * as pi from "./math.js";
+import { add, substract, multiply, divide } from "./task.js";
+// import * as add from "./task"
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
@@ -13,6 +15,15 @@ ReactDOM.render(
       {/* <li>{doublePi(2)}</li> */}
       <li>{pi.doublePi(2)}</li>
       <li>{pi.triplePi()}</li>
+    </ul>
+    <h4>Task output</h4>
+    <ul>
+      {/* <li>{add.add(1,2)}</li>
+      <li>{add.substract(9,3)}</li> */}
+      <li>{add(1, 2)}</li>
+      <li>{substract(9, 3)}</li>
+      <li>{multiply(5, 1)}</li>
+      <li>{divide(5, 2)}</li>
     </ul>
   </StrictMode>,
   rootElement
